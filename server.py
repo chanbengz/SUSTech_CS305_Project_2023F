674276accd0de3a5486b1a1c7df1157af0174beb
chanbengz <chanbengz@foxmail.com>
2023-11-19 16:51:41 +0800

    Add server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from argparse import ArgumentParser
+from threading import Thread
+
+parser = ArgumentParser()
+parser.add_argument('--ip', '-i', type=str)
+parser.add_argument('--port', '-p', type=int)
+args = parser.parse_args()
+PORT = args.port or 8080
+
+if __name__ == "__main__":
+    Thread(target=server.server_forever).start()
